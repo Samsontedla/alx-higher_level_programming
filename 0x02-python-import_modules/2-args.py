ad2df842d27115c9ed06fe5ded275b5a507871d3
samson tedla <samitedla23@gmail.com>
2021-09-09 19:46:43 +0000

    number of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) == 1:
+    print("{} arguments.".format(0))
+elif len(sys.argv) == 2:
+    print("{} argument:".format(len(sys.argv) - 1))
+    print("{}: {}".format((1), sys.argv[1]))
+elif len(sys.argv) > 2:
+    print("{} arguments:".format(len(sys.argv) - 1))
+    for i in range(0, len(sys.argv) - 1):
+        print("{}: {}".format((i + 1), sys.argv[i + 1])) 
