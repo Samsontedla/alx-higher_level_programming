43b4b6888045fa9fcfc1433c81ad51d970586251
samson tedla <samitedla23@gmail.com>
2021-09-12 20:08:40 +0000

    print list integer and element at

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 '''Function that retrives an element in c'''
+
+
 def element_at(my_list, idx):
     if idx < 0:
         return(None)
     if idx > len(my_list):
         return(None)
-    if idx:
-        return(my_list[idx])
+    for i in range(0, len(my_list)):
+        if i == idx:
+            return(my_list[i])
