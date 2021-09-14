1b15812e2eb4a69eb6a8da546c588b3f0f5f97bd
samson tedla <samitedla23@gmail.com>
2021-09-14 00:39:40 +0000

    fixed errors and added switch, lists, is palindrome, print python list info

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,9 +3,9 @@
 
 def max_integer(my_list=[]):
     '''function that finds the biggest integer of a list'''
-    biggest = 0
-    if my_list == []:
-        return
+    if len(my_list) == 0:
+        return (None)
+    biggest - my_list[0]
     for i in range(len(my_list)):
         if my_list[i] > biggest:
             biggest = my_list[i]
