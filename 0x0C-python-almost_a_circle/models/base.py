d8e10fa6787fb41d49750178ed179de33a871ab1
samson tedla <samitedla23@gmail.com>
2021-10-19 13:50:30 +0000

    more methods on base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -15,3 +15,53 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns JSON string representation"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes JSON string representation of objects to a file"""
+        if list_objs is None:
+            list_dict = []
+        else:
+            list_dict = []
+            for obj in list_objs:
+                list_dict.append(obj.to_dictionary())
+
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w') as f:
+            f.write(cls.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns list of JSON string representation"""
+        json_list = []
+        if json_string is None or len(json_string) == 0:
+            return json_list
+        else:
+            json_list = (json.loads(json_string))
+            return json_list
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance by processing a dictionary"""
+        li = []
+        dummy = cls(1, 1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances"""
+        try:
+            with open(cls.__name__ + ".json", "r") as f:
+                return [cls.create(**dictionary) for
+                        dictionary in cls.from_json_string(f.read())]
+        except FileNotFoundError:
+            return []
