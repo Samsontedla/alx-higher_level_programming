22bb44ea4744894869b0bb2745f60b508dd6da5e
samson tedla <samitedla23@gmail.com>
2021-10-19 19:12:05 +0000

    test cases

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Defines unittests for base.py"""
-import io
-import sys
+import os
 import unittest
 from models.base import Base
-
+from models.rectangle import Rectangle
+from models.square import Square
 
 class TestBase_instantiation(unittest.TestCase):
     """Unittests for testing instantiation of the Base class."""
