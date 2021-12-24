9a41c8e0a228743e14e31e65251fc0a65f6a38c3
samson tedla <samitedla23@gmail.com>
2021-12-24 19:01:33 +0000

    advanced tasks on orm

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
 """class definition of a City and an instance Base"""
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from model_state import Base, State
+from sqlalchemy.orm import relationship
 
 
 class City(Base):
-    """Represents a city for a MySQL database.
+    """City class"""
+    __tablename__ = 'cities'
 
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store Cities.
-        id (sqlalchemy.Integer): The City's id.
-        name (sqlalchemy.String): The City's name.
-    """
-    __tablename__ = "cities"
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True, unique=True)
+    id = Column(Integer, primary_key=True, unique=True,
+                autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,14 +7,9 @@ Base = declarative_base()
 
 
 class State(Base):
-    """Represents a state for a MySQL database.
+    """ base class """
+    __tablename__ = 'states'
 
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to store States.
-        id (sqlalchemy.Integer): The state's id.
-        name (sqlalchemy.String): The state's name.
-    """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True, nullable=False,
-                autoincrement=True, unique=True)
+    id = Column(Integer, primary_key=True,
+                nullable=False, unique=True)
     name = Column(String(128), nullable=False)
